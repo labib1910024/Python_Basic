ecb0ae2fde5a17c190960c802c21d4cbf629308a
Md. Sajidur Rahman Labib <87533597+labib1910024@users.noreply.github.com>
2024-05-24 01:20:26 +0600

    Add files via upload

---
+++ Test_01.py
@@ -0,0 +1,57 @@
+# print something :
+print("Hello World!") 
+
+
+# Check version :
+import sys 
+print(sys.version)
+
+
+# Conditional :
+if 5>2 :
+    print("Five is greater than two")
+
+# Type of variable :
+x = 5
+y = "Galib"
+
+print(type(x))
+print(type(y))
+
+
+# Multiple variables :
+x,y,z = "Orange", "Banana" , "Mango"
+a,b,c = 2, 3, 4
+print(x)
+print(y)
+print(z)
+
+print(a)
+print(b)
+print(c)
+
+# Another way :
+fruits = ["Orange", "Banana" , "Mango"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+
+# Global variable :
+x= "awesome"
+def myfunc():
+    print("Python is " +x)
+
+myfunc()
+
+
+
+# Local variable :
+x= "awesome"
+def myfunc():
+    x= "Awesome"
+    print("Python is " +x)
+
+myfunc()
+print("Python is " +x)
